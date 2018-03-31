280a6446c90e23d33147008d6742c4bd228de219
Sergey <ma333day@gmail.com>
2018-03-31 18:22:29 +0300

    added style modules

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import styles from './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App className={styles}/>, document.getElementById('root'));
 registerServiceWorker();
