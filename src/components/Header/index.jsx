69a7cb6bab63bf61eb775ccc80b1474eb65e66ad
Nataliia Nechiporenko <nataliia.nechiporenko@gmail.com>
2018-03-31 20:32:39 +0300

    Deleted Logo

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,16 +1,15 @@
 
 import React, { Component } from 'react';
-import Logo from '../Logo';
+
 import styles from './styles.css';
 
 export default class Header extends Component {
     render () {
         return (
             <header className={styles.header}>
-            
+
 
             </header>
         )
     }
 }
-
