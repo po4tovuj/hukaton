4ac86c260985a12a0f00b6eea1d92ea9285162f3
Miniaylo.S <po4tovuj@gmail.com>
2018-05-08 18:48:09 +0300

    Updates

--- src/components/Avatar/index.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import avatar from '../../images/ava.png';
-import styles from './styles.css';
-
-export const Ava = () => <img className={styles.ava} src={avatar} alt="" />;
